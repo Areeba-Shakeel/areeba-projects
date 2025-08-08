7a4cac0ccae62d636d874878206203c10d52920b
Areeba Shakeel <areebbarani@gmail.com>
2025-08-09 02:57:44 +0500

    index.py

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 “Python basics, mini programs, and hands-on learning.”
 <br>
 Author - Areeba Shakeel
+language - Python
---
+++ index.py
@@ -0,0 +1,7 @@
+x = 0
+x = int(input("Enter a number: "))
+if x < 0:
+    print("Negative numbers are not allowed.")
+
+
+print("Hellow World")
